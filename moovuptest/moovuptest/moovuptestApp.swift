6e564307797d461e4e3a25165ca46b6a81732303
Max <maximilliangavin@gmail.com>
2024-09-11 17:45:02 +0800

    Xcode project initial

---
+++ moovuptest/moovuptest/moovuptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  moovuptestApp.swift
+//  moovuptest
+//
+//  Created by Max on 11/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct moovuptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
